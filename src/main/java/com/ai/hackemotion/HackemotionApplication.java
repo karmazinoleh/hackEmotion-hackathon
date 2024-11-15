34cbafa0eea86d5beeb7ed891fcfd33a909bd9ae
Oleh Karmazin <kalemzwork@gmail.com>
2024-11-15 18:25:35 +0100

    init commit

---
+++ src/main/java/com/ai/hackemotion/HackemotionApplication.java
@@ -0,0 +1,13 @@
+package com.ai.hackemotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackemotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackemotionApplication.class, args);
+	}
+
+}
