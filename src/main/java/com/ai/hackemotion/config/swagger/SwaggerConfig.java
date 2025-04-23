efba5bff0f73c4e6aadf36d2d3802e8d0960574d
Oleh Karmazin <kalemzwork@gmail.com>
2025-04-23 18:16:39 +0200

    feat: swagger

---
+++ src/main/java/com/ai/hackemotion/config/swagger/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.ai.hackemotion.config.swagger;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Hackemotion")
+                        .version("v1.0.0")
+                        .description("Hackemotion API"));
+    }
+}
