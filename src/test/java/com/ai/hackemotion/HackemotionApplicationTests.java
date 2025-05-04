3e81f6fb8fc8e221bb68f369b4e7fad36e71088a
Oleh Karmazin <kalemzwork@gmail.com>
2025-05-04 17:18:10 +0200

    bugfix: specified test class

--- src/test/java/com/ai/hackemotion/HackemotionApplicationTests.java
+++ src/test/java/com/ai/hackemotion/HackemotionApplicationTests.java
@@ -3,11 +3,11 @@ package com.ai.hackemotion;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HackemotionApplicationTests.class)
 class HackemotionApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() { // context
 	}
 
 }
